2d654412bd416a2e6c88286376592fc8866ace15
Hasan-Theyab <hhdoop70@gmail.com>
2021-08-27 18:46:27 +0300

    finished step 11 in app.js

--- app.js
+++ app.js
@@ -190,17 +190,17 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scope());
+console.log(joe.scope());
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+// everything inside the constructor function (Student).
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+// everything inside the constructor function (Window). 
 // 3. Explain why "this" is different when an arrow function is used.
-//
+// arrow functions use the value of "this" in their lexical scope. This leads to very different behaviour.
